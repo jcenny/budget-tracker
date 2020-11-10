8e1fa45932678fc5e616fae005b531495ebc403a
jcenny <jcenny26@gmail.com>
2020-11-09 21:02:37 -0800

    Complete get categories functionality with drop down menu in transactions list

--- client/Components/App.jsx
+++ client/Components/App.jsx
@@ -7,18 +7,28 @@ import TransactionList from './TransactionList.jsx';
 
 function App() {
   const [transactions, setTransactions] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   const getTransactions = () => {
     Axios.get('/transactions').then((data) => {
       setTransactions(data.data);
     })
+  };
+
+  const getCategories = () => {
+    Axios.get('/categories').then((data) => {
+      setCategories(data.data);
+    })
   }
 
-  useEffect(getTransactions, [])
+  useEffect(() => {
+    getTransactions();
+    getCategories();
+  }, [])
 
   return (
     <div>
-      <TransactionList transactions={transactions}/>
+      <TransactionList transactions={transactions} categories={categories}/>
       <CategoryForm />
       <PurchaseForm />
     </div>
--- client/Components/TransactionList.jsx
+++ client/Components/TransactionList.jsx
@@ -1,23 +1,38 @@
 import React, { useState } from 'react';
 
-function TransactionList({ transactions }) {
+function TransactionList({ transactions, categories }) {
   return (
     <div>
       <table>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Category</th>
-          <th>Remaining Budget</th>
-        </tr>
-        {transactions.map((transaction) => {
-          return (
-            <tr>
-              <td>{transaction.name}</td>
-              <td>{transaction.amount}</td>
-            </tr>
-          )
-        })}
+        <thead>
+          <tr>
+            <th colSpan='4'>Transactions List and Remaining Budget</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Category</th>
+            <th>Budget</th>
+          </tr>
+          {transactions.map((transaction, index) => {
+            return (
+              <tr key={`transaction: ${index}`}>
+                <td>{transaction.name}</td>
+                <td>{transaction.amount}</td>
+                <td>
+                <select>
+                  {categories.map(({ category }, index) => (
+                    <option key={`category: ${index}`}>{category}</option>
+                  ))}
+                </select>
+                </td>
+                <td>{!transaction.budget ? '' : transaction.budget}</td>
+              </tr>
+            )
+          })}
+        </tbody>
       </table>
     </div>
   )
